b6d43963fae4058a9536e3e05910d046a6bdd90a
bradyDarren <dcgbfinancial@gmail.com>
2024-11-15 05:38:57 -0600

    addition of user inputs and calling of icon init method

---
+++ oop/main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+from icon import Icon
+import random 
+
+icon_name = input("Input the desired name of you icon: ")
+icon_color = input("Input the desired color of your icon: ")
+icon_shape = input("Input the desired shape of your icon (arrow/turtle/circle/square/triangle/classic): ")
+
+user_icon = Icon(icon_name, icon_color, icon_shape)
+
+user_icon.name = Turtle()
+
+user_icon.name.shape(icon_shape)
+user_icon.name.color(icon_color)
+
+screen = Screen()
+screen.exitonclick()
