a2c214e9a371bc975dbd01678e5cd0a387e9694e
bradyDarren <dcgbfinancial@gmail.com>
2024-11-15 05:18:16 -0600

    addition of a directory

---
+++ oop/rand_walk.py
@@ -0,0 +1,32 @@
+# will make our turtle icon generate a random path to walk, while changing color and thickness of the paths as a different direction is taken. 
+
+from turtle import Turtle, Screen
+import random
+
+t = Turtle()
+t.color("green")
+t.shape("turtle")
+
+colors = ["spring green", "cornflower blue", "gold", "deep pink", "crimson"]
+
+""" 1. generate a random color."""
+def rand_color():
+    color = random.choice(colors)
+    return color 
+""" 2. generate a random direction to move (right/left/forward/backward),  """
+
+options = [Turtle.left, Turtle.right, Turtle.forward, Turtle.backward]
+
+def rand_direct(options, steps):
+    selection = random.choice(options)
+    return selection(steps) 
+
+a = rand_direct(options, 5)
+
+t.a
+
+""" 3. Increase line thickness as each move is made. As well as change color of the line as each move is made. """
+
+screen = Screen()
+screen.exitonclick()
+
