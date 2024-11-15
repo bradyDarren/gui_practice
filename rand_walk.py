959a1f2dd7ae098b8a37c3017fa99524a49f2d1b
Darren Brady <dcgbfinancial@gmail.com>
2024-11-15 09:51:21 +0000

    setting goals and expected outcomes of the program

--- rand_walk.py
+++ rand_walk.py
@@ -1,1 +1,22 @@
+# will make our turtle icon generate a random path to walk, while changing color and thickness of the paths as a different direction is taken. 
+
+from turtle import Turtle, Screen
+
+t = Turtle()
+t.color("green")
+t.shape("turtle")
+
+colors = ["spring green", "cornflower blue", "gold", "deep pink", "crimson"]
+
+""" 1. generate a random color."""
+random.choice(colors)
+""" 2. generate a random direction to move (right/left/forward/backward),  """
+
+""" 3. Increase line thickness as each move is made. As well as change color of the line as each move is made. """
+
+
+
+
+screen = Screen()
+screen.exitonclick()
 
