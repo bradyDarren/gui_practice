efd4285a613920f2dd15a5ae8e79d387e50394f4
bradyDarren <dcgbfinancial@gmail.com>
2024-11-28 12:58:52 -0600

    addition of a few test lines

--- oop/rand_walk.py
+++ oop/rand_walk.py
@@ -5,28 +5,25 @@ import random
 
 t = Turtle()
 t.color("green")
+print(t.shape())
 t.shape("turtle")
+print(t.shape())
 
-colors = ["spring green", "cornflower blue", "gold", "deep pink", "crimson"]
+# colors = ["spring green", "cornflower blue", "gold", "deep pink", "crimson"]
 
-""" 1. generate a random color."""
-def rand_color():
-    color = random.choice(colors)
-    return color 
-""" 2. generate a random direction to move (right/left/forward/backward),  """
+# """ 1. generate a random color."""
+# def rand_color():
+#     color = random.choice(colors)
+#     return color 
+# """ 2. generate a random direction to move (right/left/forward/backward),  """
 
-options = [Turtle.left, Turtle.right, Turtle.forward, Turtle.backward]
+# # options = [, Turtle.right, Turtle.forward, Turtle.backward]
 
-def rand_direct(options, steps):
-    selection = random.choice(options)
-    return selection(steps) 
+# def rand_direct(options, steps):
+#     selection = random.choice(options)
+#     return selection(steps) 
+# """ 3. Increase line thickness as each move is made. As well as change color of the line as each move is made. """
 
-a = rand_direct(options, 5)
-
-t.a
-
-""" 3. Increase line thickness as each move is made. As well as change color of the line as each move is made. """
-
-screen = Screen()
-screen.exitonclick()
+# screen = Screen()
+# screen.exitonclick()
 
