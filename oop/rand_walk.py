cbaf68a77787931a03e606a733aa51867f4e72cb
bradyDarren <dcgbfinancial@gmail.com>
2024-11-29 06:54:13 -0600

    addition of classes

--- oop/rand_walk.py
+++ oop/rand_walk.py
@@ -3,40 +3,42 @@
 from turtle import Turtle, Screen
 import random
 
-t = Turtle()
-t.color("green")
-t.shape("turtle")
+# t = Turtle()
+# t.color("green")
+# t.shape("turtle")
 
-colors = ["spring green", "cornflower blue", "gold", "deep pink", "crimson"]
 
 """ 1. generate a random color."""
-def rand_color():
-    color = random.choice(colors)
-    return color 
+class Colors:
+    color_choice = ["spring green", "cornflower blue", "gold", "deep pink", "crimson"]
+
+    def rand_color(self):
+        color = random.choice(self.color_choice)
+        return color 
 
 """function created to make sure we account for each directional movement passed into our turtle object."""
+class Directions:
+    name = ""
 
-def right(name):
-    name.right(90)
+    def right(self):
+        self.name.right(90)
 
-def left(name):
-    name.left(90)
+    def left(self, name):
+        name.left(90)
 
-def forward(name):
-    name.forward(10)
+    def forward(self, name):
+        name.forward(200)
 
-def backward(name):
-    name.backward(10)
+    def backward(self, name):
+        name.backward(200)
 
 """ 2. generate a random direction to move (right/left/forward/backward),  """
+class RandDirection:
+    options = [Directions.right, Directions.left, Directions.forward, Directions.backward]
 
-options = [right, left, forward, backward]
-
-def rand_direct(choices):
-    selection = random.choice(options)
-    return selection()
-
-rand_direct(options)
+    def rand_direct(self, choices):
+        selection = random.choice(choices)
+        return selection()
 
 """ 3. Increase line thickness as each move is made. As well as change color of the line as each move is made. """
 
