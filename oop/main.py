1255837e8a1efff34fb034640333ccc461c004ea
bradyDarren <dcgbfinancial@gmail.com>
2024-12-05 06:03:52 -0600

    addition of the loop to test the random command passed into our Turtle() object.

--- oop/main.py
+++ oop/main.py
@@ -6,21 +6,23 @@ icon_name = input("Input the desired name of you icon: ")
 icon_color = input("Input the desired color of your icon: ")
 icon_shape = input("Input the desired shape of your icon (arrow/turtle/circle/square/triangle/classic): ")
 
+"""passing in the responses from the user."""
 user_icon = Icon(icon_name, icon_color, icon_shape)
 
+"""assigning the user input for name and assigning it to a Turtle() object."""
 user_icon.name = Turtle()
 
+"""passing in the user input to designate the shape and color of our Turle() object. """
 user_icon.name.shape(icon_shape)
 user_icon.name.color(icon_color)
 
-user_icon.name.right(90)
+# user_icon.name.right(90)
 
 path = RandDirection(user_icon.name)
 
-print(path().name)
+for _ in range(200):
+   path.rand_direct()
 
-# for _ in range(50):
-#     path.rand_direct()
 
-# screen = Screen()
-# screen.exitonclick()
+screen = Screen()
+screen.exitonclick()
